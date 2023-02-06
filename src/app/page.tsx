6ca2f3a4c1cb05206fc2bcfbe7a94d50064ca455
saix511 <saadirfan41@gmail.com>
2023-02-06 21:39:00 +0500

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,216 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import CheckIcon from './checkIcon'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const hobbyFeatures = [
+  'Lorem ipsum dolor sit',
+  'Lorem ipsum dolor sit',
+  'Lorem ipsum dolor sit',
+]
+const scaleFeatures = [
+  'Lorem ipsum dolor sit',
+  'Lorem ipsum dolor sit',
+  'Lorem ipsum dolor sit',
+]
+const growthFeatures = [
+  'Lorem ipsum dolor sit, amet consectetur.',
+  'Lorem ipsum dolor sit, amet consectetur.',
+  'Lorem ipsum dolor sit, amet consectetur.',
+  'Lorem ipsum dolor sit, amet consectetur.',
+  'Lorem ipsum dolor sit, amet consectetur.',
+]
+const Home = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='bg-gray-900'>
+      <div className='px-6 pt-12 lg:px-8 lg:pt-20'>
+        <div className='text-center'>
+          <h2 className='text-xl font-semibold leading-6 text-gray-300'>
+            Pricing
+          </h2>
+          <p className='mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl lg:text-5xl'>
+            The right price for you, whoever you are
+          </p>
+          <p className='mx-auto mt-3 max-w-4xl text-xl text-gray-300 sm:mt-5 sm:text-2xl'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit
+            numquam eligendi quos odit doloribus molestiae voluptatum.
+          </p>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      <div className='mt-16 bg-white pb-12 lg:mt-20 lg:pb-20'>
+        <div className='relative z-0'>
+          <div className='absolute inset-0 h-5/6 bg-gray-900 lg:h-2/3' />
+          <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+            <div className='relative lg:grid lg:grid-cols-7'>
+              <div className='mx-auto max-w-md lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3 lg:mx-0 lg:max-w-none'>
+                <div className='flex h-full flex-col overflow-hidden rounded-lg shadow-lg lg:rounded-none lg:rounded-l-lg'>
+                  <div className='flex flex-1 flex-col'>
+                    <div className='bg-white px-6 py-10'>
+                      <div>
+                        <h3
+                          className='text-center text-2xl font-medium text-gray-900'
+                          id='tier-hobby'
+                        >
+                          Hobby
+                        </h3>
+                        <div className='mt-4 flex items-center justify-center'>
+                          <span className='flex items-start px-3 text-6xl tracking-tight text-gray-900'>
+                            <span className='mt-2 mr-2 text-4xl font-medium tracking-tight'>
+                              $
+                            </span>
+                            <span className='font-bold'>79</span>
+                          </span>
+                          <span className='text-xl font-medium text-gray-500'>
+                            /month
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='flex flex-1 flex-col justify-between border-t-2 border-gray-100 bg-gray-50 p-6 sm:p-10 lg:p-6 xl:p-10'>
+                      <ul role='list' className='space-y-4'>
+                        {hobbyFeatures.map((feature) => (
+                          <li key={feature} className='flex items-start'>
+                            <div className='flex-shrink-0'>
+                              <CheckIcon />
+                            </div>
+                            <p className='ml-3 text-base font-medium text-gray-500'>
+                              {feature}
+                            </p>
+                          </li>
+                        ))}
+                      </ul>
+                      <div className='mt-8'>
+                        <div className='rounded-lg shadow-md'>
+                          <a
+                            href='#'
+                            className='block w-full rounded-lg border border-transparent bg-white px-6 py-3 text-center text-base font-medium text-indigo-600 hover:bg-gray-50'
+                            aria-describedby='tier-hobby'
+                          >
+                            Start your trial
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='mx-auto mt-10 max-w-lg lg:col-start-3 lg:col-end-6 lg:row-start-1 lg:row-end-4 lg:mx-0 lg:mt-0 lg:max-w-none'>
+                <div className='relative z-10 rounded-lg shadow-xl'>
+                  <div
+                    className='pointer-events-none absolute inset-0 rounded-lg border-2 border-indigo-600'
+                    aria-hidden='true'
+                  />
+                  <div className='absolute inset-x-0 top-0 translate-y-px transform'>
+                    <div className='flex -translate-y-1/2 transform justify-center'>
+                      <span className='inline-flex rounded-full bg-indigo-600 px-4 py-1 text-base font-semibold text-white'>
+                        Most popular
+                      </span>
+                    </div>
+                  </div>
+                  <div className='rounded-t-lg bg-white px-6 pt-12 pb-10'>
+                    <div>
+                      <h3
+                        className='text-center text-3xl font-semibold tracking-tight text-gray-900 sm:-mx-6'
+                        id='tier-growth'
+                      >
+                        Growth
+                      </h3>
+                      <div className='mt-4 flex items-center justify-center'>
+                        <span className='flex items-start px-3 text-6xl tracking-tight text-gray-900 sm:text-6xl'>
+                          <span className='mt-2 mr-2 text-4xl font-medium tracking-tight'>
+                            $
+                          </span>
+                          <span className='font-bold'>149</span>
+                        </span>
+                        <span className='text-2xl font-medium text-gray-500'>
+                          /month
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='rounded-b-lg border-t-2 border-gray-100 bg-gray-50 px-6 pt-10 pb-8 sm:px-10 sm:py-10'>
+                    <ul role='list' className='space-y-4'>
+                      {growthFeatures.map((feature) => (
+                        <li key={feature} className='flex items-start'>
+                          <div className='flex-shrink-0'>
+                            <CheckIcon />
+                          </div>
+                          <p className='ml-3 text-base font-medium text-gray-500'>
+                            {feature}
+                          </p>
+                        </li>
+                      ))}
+                    </ul>
+                    <div className='mt-10'>
+                      <div className='rounded-lg shadow-md'>
+                        <a
+                          href='#'
+                          className='block w-full rounded-lg border border-transparent bg-indigo-600 px-6 py-4 text-center text-xl font-medium leading-6 text-white hover:bg-indigo-700'
+                          aria-describedby='tier-growth'
+                        >
+                          Start your trial
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='mx-auto mt-10 max-w-md lg:col-start-6 lg:col-end-8 lg:row-start-2 lg:row-end-3 lg:m-0 lg:max-w-none'>
+                <div className='flex h-full flex-col overflow-hidden rounded-lg shadow-lg lg:rounded-none lg:rounded-r-lg'>
+                  <div className='flex flex-1 flex-col'>
+                    <div className='bg-white px-6 py-10'>
+                      <div>
+                        <h3
+                          className='text-center text-2xl font-medium text-gray-900'
+                          id='tier-scale'
+                        >
+                          Scale
+                        </h3>
+                        <div className='mt-4 flex items-center justify-center'>
+                          <span className='flex items-start px-3 text-6xl tracking-tight text-gray-900'>
+                            <span className='mt-2 mr-2 text-4xl font-medium tracking-tight'>
+                              $
+                            </span>
+                            <span className='font-bold'>349</span>
+                          </span>
+                          <span className='text-xl font-medium text-gray-500'>
+                            /month
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='flex flex-1 flex-col justify-between border-t-2 border-gray-100 bg-gray-50 p-6 sm:p-10 lg:p-6 xl:p-10'>
+                      <ul role='list' className='space-y-4'>
+                        {scaleFeatures.map((feature) => (
+                          <li key={feature} className='flex items-start'>
+                            <div className='flex-shrink-0'>
+                              <CheckIcon />
+                            </div>
+                            <p className='ml-3 text-base font-medium text-gray-500'>
+                              {feature}
+                            </p>
+                          </li>
+                        ))}
+                      </ul>
+                      <div className='mt-8'>
+                        <div className='rounded-lg shadow-md'>
+                          <a
+                            href='#'
+                            className='block w-full rounded-lg border border-transparent bg-white px-6 py-3 text-center text-base font-medium text-indigo-600 hover:bg-gray-50'
+                            aria-describedby='tier-scale'
+                          >
+                            Start your trial
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
+
+export default Home
